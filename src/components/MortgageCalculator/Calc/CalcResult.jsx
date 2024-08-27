716cca212ea9f9d6529f766fa30b8c8b0888206c
Joshua Schoonover <joshua.schoonover@austincc.edu>
2024-08-26 22:30:37 -0400

    uncalculated text in result panel

--- src/components/MortgageCalculator/Calc/CalcResult.jsx
+++ src/components/MortgageCalculator/Calc/CalcResult.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../MortgageCalculator/style.scss";
 
 
 const CalcResult = ({...props}) => {
+    
+    const [showResults, setShowResults] = useState(false)
+
 
     return (
         <div className="Parent RightBox">
+        {showResults?<>
             <h3>Your results</h3>
-            <p>Your results are shown below based on teh information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
+            <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
 
             <div className='TotalBox'>
                 <span>Your monthly repayments</span>
@@ -18,6 +22,12 @@ const CalcResult = ({...props}) => {
                 <span>Total you'll repay over the term</span>
                 <h4>$123,456.78</h4>
             </div>
+        </>:<>         
+            <span>put the image here</span>
+            <h3>Results shown here</h3>
+            <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+
+        </>}
         </div>
     )
 }
