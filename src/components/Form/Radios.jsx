ed98d0f09cc3e0703587716118ee0aea8d4f18fa
Joshua Schoonover <joshua.schoonover@austincc.edu>
2024-09-01 19:04:56 -0400

    fixed radio button active state clear error

--- src/components/Form/Radios.jsx
+++ src/components/Form/Radios.jsx
@@ -3,16 +3,6 @@ import "./Radios.scss"
 
  const Radios = ({ id, label, options, invalidFeedback, invalid, ...props}) => {
 
-    const [active, setActive] = useState(null);
-
-    const changeActive = (index) => {
-        setActive(index)
-    }
-
-    const isActive = (index) => {
-        return (index==active)?"Active":""
-    }
-
     const checkInvalid = () => {return invalid?"Invalid":""}
 
 
@@ -21,9 +11,9 @@ import "./Radios.scss"
         <label className='text-4 text-slate-700'>Mortgage Type</label>
         
         {options.map((option,index)=>(
-            <div key={"div"+id+index} className={"form-check px-5 py-2 my-2 border border-slate-500 rounded-1 "+isActive(index)} {...props}>
-                <input onChange={()=>changeActive(index)} key={"input"+id+index} className={"form-check-input border-slate-700 "+isActive(index)} type="radio" id={""+id+index} name={id} value={option} />
-                <label key={"label"+id+index} className={"form-check-label text-3 text-slate-900 "+isActive(index)} htmlFor={""+id+index} >{option}</label>
+            <div key={"div"+id+index} className="form-check px-5 py-2 my-2 border border-slate-500 rounded-1" {...props}>
+                <input key={"input"+id+index} className="form-check-input border-slate-700" type="radio" id={""+id+index} name={id} value={option} />
+                <label key={"label"+id+index} className="form-check-label text-3 text-slate-900" htmlFor={""+id+index} >{option}</label>
             </div>
         ))}
 
